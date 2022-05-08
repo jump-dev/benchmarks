cadb006e1cfcd040a9b3aef84b5b2ea1e6bc0ca2
Oscar Dowson <odow@users.noreply.github.com>
2022-05-08 17:28:33 +1200

    Add PowerModels benchmark (#7)

---
+++ src/latency/power_models_opf_acp.jl
@@ -0,0 +1,21 @@
+# Copyright (c) 2022: jump-dev/benchmarks contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+import Ipopt
+import JuMP
+import PowerModels
+
+function main(case::String)
+    data = PowerModels.parse_file(joinpath(@__DIR__, "data", case * ".m"))
+    solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6)
+    result = PowerModels.run_opf(data, PowerModels.ACPPowerModel, solver)
+    println()
+    println(result["termination_status"])
+    println(result["primal_status"])
+    println(result["solve_time"])
+    return
+end
+
+main(ARGS[1])
