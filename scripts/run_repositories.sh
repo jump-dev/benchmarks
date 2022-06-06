a8d9373182514022779ee5f9e730bd72845099c6
odow <o.dowson@gmail.com>
2022-06-06 16:55:47 +1200

    Remove bad runs

--- scripts/run_repositories.sh
+++ scripts/run_repositories.sh
@@ -10,12 +10,10 @@ cd ~/Code/benchmarks
 # Update the repository
 git pull
 
-# Run the new benchmarks
-~/julia --project=. scripts/benchmarks.jl --checkout master
-~/julia --project=. scripts/benchmarks.jl --run
-~/julia --project=. scripts/benchmarks.jl --publish
+# Update the repository activity
+~/julia --project=. scripts/repositories.jl --update
 
 # Save and re-upload
 git add .
-git commit -m "Automatic update"
+git commit -m "Repositories: automatic update"
 git push
