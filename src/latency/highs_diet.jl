d9ea0dedb4f891ba4d036f169cb564d91dbd7526
Oscar Dowson <odow@users.noreply.github.com>
2023-12-22 15:05:50 +1300

    Update highs_diet.jl

--- src/latency/highs_diet.jl
+++ src/latency/highs_diet.jl
@@ -8,28 +8,28 @@ import HiGHS
 
 function main()
     category_data = [
-        1800.0 2200.0;
-          91.0    Inf;
-           0.0   65.0;
-           0.0 1779.0
+        1800.0 2200.0
+        91.0 Inf
+        0.0 65.0
+        0.0 1779.0
     ]
     cost = [2.49, 2.89, 1.50, 1.89, 2.09, 1.99, 2.49, 0.89, 1.59]
     food_data = [
-        410 24 26 730;
-        420 32 10 1190;
-        560 20 32 1800;
-        380  4 19 270;
-        320 12 10 930;
-        320 15 12 820;
-        320 31 12 1230;
-        100  8 2.5 125;
-        330  8 10 180
+        410 24 26 730
+        420 32 10 1190
+        560 20 32 1800
+        380 4 19 270
+        320 12 10 930
+        320 15 12 820
+        320 31 12 1230
+        100 8 2.5 125
+        330 8 10 180
     ]
     model = Model(HiGHS.Optimizer)
     set_silent(model)
     @variable(
         model,
-        nutrition[i=1:size(category_data, 1)],
+        nutrition[i = 1:size(category_data, 1)],
         lower_bound = category_data[i, 1],
         upper_bound = category_data[i, 2],
     )
